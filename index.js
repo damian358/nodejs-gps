15e20a4cbe828e700bfb5e2affb66c1dbcc161bf
gunther <damian.debicki@gmail.com>
2015-11-19 16:07:34 +0100

    working serialport communication for custom arduino device

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var com = require("serialport");
+var SerialPort = require("serialport").SerialPort;
+
+
+require("serialport").list(function (err, ports) {
+    ports.forEach(function (port) {
+        if (port.pnpId) {
+            console.log(port.comName);
+            console.log(port.pnpId);
+            console.log(port.manufacturer);
+        }
+    });
+});
+
+var serialPort = new SerialPort("/dev/ttyACM0", {
+    baudrate: 115200,
+    parser: com.parsers.readline('\r\n')
+});
+
+serialPort.on('open', function () {
+    console.log('Port open');
+});
+
+serialPort.on('data', function (data) {
+    console.log(data);
+});
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
